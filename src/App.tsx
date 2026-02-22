03861cba980fc0dda344150dc6b60691a24b6e03
Arrowhead Flies <hello@arrowheadflies.com>
2026-02-22 16:17:26 -0600

    Fix custom domain: add persistent CNAME and reset base path

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Hero from './components/Hero'
 import Story from './components/Story'
 import EmailCapture from './components/EmailCapture'
